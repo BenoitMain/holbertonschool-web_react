2893b47a7fdff4e160a94df5edee2a1f1d6aa23e
BenoitMain <benoitmaingon@gmail.com>
2025-09-10 11:31:39 +0200

    Add Directors interface and director1 instance to main.ts

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -23,5 +23,18 @@ const teacher3: Teacher = {
     contract: false,
 };
 
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
 console.log(teacher2);
 console.log(teacher3);
+console.log(director1);
