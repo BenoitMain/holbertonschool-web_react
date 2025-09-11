34e6b2d4cc19374dba838428a198cd3fcdfad68e
BenoitMain <benoitmaingon@gmail.com>
2025-09-11 11:47:54 +0200

    Refactor CRUD function signatures to enforce type checking for RowElement and RowID

--- task_3/js/crud.d.ts
+++ task_3/js/crud.d.ts
@@ -1,15 +1,6 @@
-export function insertRow(row) {
-  console.log('Insert row', row);
-  return Math.floor(Math.random() * Math.floor(1000));
-}
+import { RowID, RowElement } from './interface';
 
-export function deleteRow(rowId) {
-  console.log('Delete row id', rowId);
-  return;
-}
+export function insertRow(row: RowElement): RowID;
+export function deleteRow(rowId: RowID): void;
+export function updateRow(rowId: RowID, row: RowElement): RowID;
 
-export function updateRow(rowId, row) {
-  console.log(`Update row ${rowId}`, row);
-
-  return rowId;
-}
